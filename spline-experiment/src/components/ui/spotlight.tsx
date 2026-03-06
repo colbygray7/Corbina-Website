dbb98ae821f6a60fc566ec8fc1e0572dd1da40d9
Colby Gray <Colby.gray7@gmail.com>
2026-03-05 21:45:03 -0800

    chore: Remove spline-experiment project and update the root index.html.

--- spline-experiment/src/components/ui/spotlight.tsx
+++
@@ -1,5 +0,0 @@
-// Re-export Aceternity Spotlight as default for the demo
-export { Spotlight } from '@/components/aceternity/spotlight';
-
-// Also export ibelick version if needed
-export { Spotlight as SpotlightInteractive } from '@/components/ibelick/spotlight';
